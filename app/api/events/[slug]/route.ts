57433d6c65e5d6756ac793e3a5fc5b39b32f0573
alokkkxpixel <alokpithale4@gmail.com>
2025-12-01 17:05:51 +0530

    get single event by slug api created

---
+++ app/api/events/[slug]/route.ts
@@ -0,0 +1,55 @@
+import connectDB from "@/lib/mongodb";
+import Event from "@/database/event.model";
+import { NextRequest, NextResponse } from "next/server";
+
+
+type RouteParams = {
+    params: {
+        slug: string
+    }
+}
+
+
+
+// GET /api/events/[slug]
+ //fetch a single event by slug 
+
+
+ export async function GET(req: NextRequest, { params }: RouteParams): Promise<NextResponse> {
+
+    try {
+
+        await connectDB()
+        const { slug } = await params // if the folder name is [id] then we detruct {id} = params
+        console.log("params" , params)
+ 
+
+        console.log(slug,"slug")
+
+        if (!slug || typeof slug !== "string"
+            || slug.trim() === ""
+        ) {
+            return NextResponse.json({ message: "slug is required" }, { status: 400 })
+        }
+
+         const sanitizedSlug = slug.trim().toLowerCase()
+
+        const event = await Event.findOne({ slug:sanitizedSlug }).lean()
+
+
+        // if not event is found we return this below error message
+        if (!event) {
+            return NextResponse.json({ message: "Event not found" }, { status: 404 })
+        }
+
+        return NextResponse.json({message:"Event fetching successfully", event}, { status: 200 })
+
+    } catch (e) {
+        return NextResponse.json(
+            { message: "Server Error ", error: e instanceof Error ? e.message : "unknown" },
+            { status: 400 }
+        )
+
+    }
+
+}
--- components/FeaturedEvents.tsx
+++ components/FeaturedEvents.tsx
@@ -26,7 +26,7 @@ const FeaturedEvents = async () => {
     <div className='mt-20 space-y-7' id='events'>
         <h3>Featured Events</h3>
       <ul className='events list-none'>
-       {events.map((event:IEvent) => (
+       {events && events.length > 0 &&  events.map((event:IEvent) => (
         <li key={event.title}>
 
             <EventCard  {...event}/>
