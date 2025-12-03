213f634b790ee9a533bdd35b996be1095d9ecafc
alokkkxpixel <alokpithale4@gmail.com>
2025-12-03 19:24:16 +0530

    add the Proxy (middleware) for protect the route

---
+++ proxy.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export function proxy(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    return NextResponse.next();
+  } catch (err) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
