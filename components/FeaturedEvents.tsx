3a2b7c1efc032ac74d3dfdbc26819a2fa20e567c
alokkkxpixel <alokpithale4@gmail.com>
2025-12-04 20:51:25 +0530

    events static

--- components/FeaturedEvents.tsx
+++ components/FeaturedEvents.tsx
@@ -1,21 +1,20 @@
 import { IEvent } from '@/database'
 import EventCard from './EventCard'
-import events from '@/lib/constants'
+import events from '@/lib/constants' assert { type: 'json' }
 import axios from 'axios'
 import { cacheLife } from 'next/cache'
+
 const FeaturedEvents = async () => {
 
 "use cache"
 cacheLife('hours')
 
-   const res = await fetch(`/api/events`)
-   const data = await res.json()
+  //  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`)
+  //  const data = await res.json()
  
 
-   const events = data.event
-  
-  
-      
+  //  const events = data.event
+
   return (
 
    <>
@@ -23,7 +22,7 @@ cacheLife('hours')
     <div className='mt-20 space-y-7' id='events'>
         <h3>Featured Events</h3>
       <ul className='events list-none'>
-       {events && events.length > 0 &&  events.map((event:IEvent) => (
+       {events && events.length > 0 &&  events.map((event) => (
         <li key={event.title}>
 
             <EventCard  {...event}/>
